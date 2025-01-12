46102586d5e9933fcb9f8e8a0fedfd4ff92ccf39
William White <willw@novxsystems.com>
2025-01-12 14:00:39 -0500

    Initial Commit

---
+++ app/src/main/java/com/soundpaletteui/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.soundpaletteui.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.soundpaletteui.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
