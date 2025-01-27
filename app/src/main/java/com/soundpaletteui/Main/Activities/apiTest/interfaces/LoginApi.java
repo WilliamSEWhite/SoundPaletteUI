8be3ef96529ff0abc095cb2eebcbf6f83286036f
James Windjack <jw17rm@brocku.ca>
2025-01-27 16:34:50 -0500

    URL fix for test

--- app/src/main/java/com/soundpaletteui/Main/Activities/apiTest/interfaces/LoginApi.java
+++ app/src/main/java/com/soundpaletteui/Main/Activities/apiTest/interfaces/LoginApi.java
@@ -8,7 +8,7 @@ import retrofit2.http.Query;
 
 public interface LoginApi {
 
-    @GET("api/login/register")
+    @GET("api/login/login-user")
     Call<User> loginUser(
             @Query("username") String username,
             @Query("password") String password
