0b66e72466acc2cd2dc65c9cfec7d4edc8e501e5
Victoria Danh <80844920+vd19qe@users.noreply.github.com>
2025-02-26 18:26:00 -0500

    PostAdapter.java updated with comments

--- app/src/main/java/com/soundpaletteui/Activities/Posts/PostAdapter.java
+++ app/src/main/java/com/soundpaletteui/Activities/Posts/PostAdapter.java
@@ -1,6 +1,8 @@
 package com.soundpaletteui.Activities.Posts;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.soundpaletteui.Activities.Interactions.CommentBottomSheet;
 import com.soundpaletteui.Infrastructure.Models.PostModel;
 import com.soundpaletteui.Infrastructure.Models.PostContentModel;
 import com.soundpaletteui.R;
@@ -27,6 +31,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
         this.postList = postList;
     }
 
+    // Create the main View using adapter_posts.xml
     @NonNull
     @Override
     public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -35,46 +40,61 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
         return new PostViewHolder(view);
     }
 
+    // Binds (sets) the objects in adapter_posts.xml
     @Override
     public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
         PostModel post = postList.get(position);
-        holder.postUsername.setText(post.getUsername());
-        holder.postCaption.setText(post.getPostCaption());
-
-        holder.postFragmentDisplay.removeAllViews(); // Clear previous content
+        int postId = post.getPostId();
+        holder.postUsername.setText(post.getUsername());        // Get username from Post
+        holder.postCaption.setText(post.getPostCaption());      // Get caption from Post
+        holder.postFragmentDisplay.removeAllViews();            // Clear previous content
         LayoutInflater inflater = LayoutInflater.from(context);
         View fragmentView;
 
-        if (post.getPostType() == TEXT_POST) { // Text Post
+        // Sets the fragmentView in adapter_posts based on the postType
+        if (post.getPostType() == TEXT_POST) {                  // Text Post
             fragmentView = inflater.inflate(R.layout.adapter_posts_text, holder.postFragmentDisplay, false);
             TextView postTextDisplay = fragmentView.findViewById(R.id.postTextDisplay);
             postTextDisplay.setText(post.getPostContent().getPostContent());
-        } else if (post.getPostType() == IMAGE_POST) { // Image Post
+        } else if (post.getPostType() == AUDIO_POST) {          //Audio Post
+            fragmentView = new View(context);
+        } else if (post.getPostType() == IMAGE_POST) {          // Image Post
             fragmentView = inflater.inflate(R.layout.adapter_posts_image, holder.postFragmentDisplay, false);
             ImageView postImageDisplay = fragmentView.findViewById(R.id.postImageDisplay);
             String imageName = post.getPostContent().getPostContent().replace(".png", "").replace(".jpg", "");
             int imageResource = holder.itemView.getContext().getResources().getIdentifier(imageName, "drawable", holder.itemView.getContext().getPackageName());
             postImageDisplay.setImageResource(imageResource);
-        } else {
-            fragmentView = new View(context); // Fallback empty view
+        } else {                                                // Empty View
+            fragmentView = new View(context);
         }
 
+        // Like Button Actions
         holder.likeButton.setOnClickListener(v -> {
             if (holder.isLiked) {
                 holder.likeButton.setImageResource(R.drawable.post_fav_empty_24);
+                Log.d("Like Button", "Unliked Post ID#" +postId);
             } else {
                 holder.likeButton.setImageResource(R.drawable.post_fav_filled_24);
+                Log.d("Like Button", "Liked Post ID#" +postId);
             }
             holder.isLiked = !holder.isLiked;
         });
-        holder.commentButton.setOnClickListener(v ->
-                Toast.makeText(v.getContext(), "Commented on Post", Toast.LENGTH_SHORT).show()
-        );
+
+        // Comment Button Actions
+        holder.commentButton.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), CommentBottomSheet.class);
+            intent.putExtra("postId", position); // Use position as a placeholder postId
+            v.getContext().startActivity(intent);
+        });
+
+        // Save Button Actions
         holder.saveButton.setOnClickListener(v -> {
             if (holder.isSaved) {
                 holder.saveButton.setImageResource(R.drawable.post_saved_empty_24);
+                Log.d("Save Button", "Unsaved Post ID#" +postId);
             } else {
                 holder.saveButton.setImageResource(R.drawable.post_saved_filled_24);
+                Log.d("Save Button", "Saved Post ID#" +postId);
             }
             holder.isSaved = !holder.isSaved;
         });
@@ -87,6 +107,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
         return postList.size();
     }
 
+    // Set and find all objects in adapter_posts.xml
     public static class PostViewHolder extends RecyclerView.ViewHolder {
         TextView postUsername, postCaption;
         ViewGroup postFragmentDisplay;
