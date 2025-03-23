824a74f78ad8bfce41dc1affa1fa3fa3dad3486f
William White <willw@novxsystems.com>
2025-03-23 15:31:04 -0400

    Changed name of chatroom models

--- app/src/main/java/com/soundpaletteui/Infrastructure/Models/NewChatroomModel.java
+++ app/src/main/java/com/soundpaletteui/Infrastructure/Models/NewChatroomModel.java
@@ -4,13 +4,13 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class NewChatRoomModel {
+public class NewChatroomModel {
     @SerializedName("Name")
     public String name;
     @SerializedName("Users")
     public List<String> users;
 
-    public NewChatRoomModel(String name, List<String> users)
+    public NewChatroomModel(String name, List<String> users)
     {
         this.name = name;
         this.users = users;
