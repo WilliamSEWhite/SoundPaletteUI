cec634d883de28cff5da6d94ff3db268ad585fa2
Victoria Danh <80844920+vd19qe@users.noreply.github.com>
2025-03-31 19:22:56 -0400

    Modified ChatClient and ChatApiEndpoints for updating chatrooms

--- app/src/main/java/com/soundpaletteui/Infrastructure/ApiEndpoints/ChatApiEndpoints.java
+++ app/src/main/java/com/soundpaletteui/Infrastructure/ApiEndpoints/ChatApiEndpoints.java
@@ -5,6 +5,9 @@ import com.soundpaletteui.Infrastructure.Models.ChatroomModel;
 import com.soundpaletteui.Infrastructure.Models.ChatroomModelLite;
 import com.soundpaletteui.Infrastructure.Models.NewChatroomModel;
 import com.soundpaletteui.Infrastructure.Models.NewMessageModel;
+import com.soundpaletteui.Infrastructure.Models.UpdateChatroomModel;
+import com.soundpaletteui.Infrastructure.Models.UserProfileModel;
+import com.soundpaletteui.Infrastructure.Models.UserProfileModelLite;
 
 import java.util.List;
 
@@ -21,11 +24,14 @@ public interface ChatApiEndpoints {
     Call<List<ChatMessageModel>> getMessagesForChatroom(@Query("chatroomId") int chatroomId);
     @GET("api/chat/get-private-chatroom")
     Call<ChatroomModelLite> getPrivateChatroom(@Query("userId") int userId, @Query("username") String username);
+    @GET("api/chat/get-user-chatrooms")
+    Call<List<String>> getChatroomMembers(@Query("chatroomId") int chatroomId);
     @POST("api/chat/create-chatroom")
     Call<ChatroomModelLite> createChatroom(@Body NewChatroomModel newChatRoom);
     @POST("api/chat/send-message")
     Call<Void> sendMessage(@Body NewMessageModel newMessage);
     @GET("api/chat/remove-user-from-chat")
     Call<Void> removeUserFromChatroom(@Query("chatroomId") int chatroomId, @Query("userId") int userId);
-
+    @POST("api/chat/update-chatroom")
+    Call<Void> updateChatroom(@Body UpdateChatroomModel request);
 }
